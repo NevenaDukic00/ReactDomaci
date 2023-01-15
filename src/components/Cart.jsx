38458dc9f6bfc501677745fd1a6f857a0ea04aa8
NevenaDukic00 <nevenadukic.business@outlook.com>
2023-01-15 20:32:40 +0100

    Form

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import SingleProduct from './SingleProduct';
 import $ from "jquery"
 import jquery from "jquery"
+import FormMy from './FormMy';
 
 function Cart({numProducts,products}) {
 
@@ -13,41 +14,6 @@ function Cart({numProducts,products}) {
     
      return price;
   }
- 
-  jquery(function () {
-   
-
-    $("#form").on("submit", function (e) {
-      e.preventDefault();
-      if (
-        $("#firstname").val() &&
-        $("#lastname").val() &&
-        $("#email").val() &&
-        $("#address").val()
-      ) {
-        
-        alert(
-          "<Shipping details>" +
-            "\nFirstname: " +
-            $("#firstname").val() +
-            "\nLastname: " +
-            $("#lastname").val() +
-            "\nEmail: " +
-            $("#email").val() +
-            "\nAddress: " +
-            $("#address").val() +
-            "\nTotal price: " +
-            totalPrice() +
-            "$" +
-            "\nTotal number of products: " +
-            numProducts
-        );
-      } else {
-        alert("All fields are required!");
-      }
-    });
-  });
- 
 
   console.log("Na stanju:" + numProducts);
     if (numProducts === 0) {
@@ -70,51 +36,12 @@ function Cart({numProducts,products}) {
             />
           })}
           </div>
-          
-          <form action="." id="form"  >
-            <h1 className="headerCart">Cart</h1>
-            <div className="formCart">
-              <label htmlFor="firstname">Firstname:</label>
-              <input
-                type="text"
-                placeholder="Enter firstname"
-                name="firstname"
-                id="firstname"
-              ></input>
 
-              <label htmlFor="lastname">Lastname:</label>
-              <input
-                type="text"
-                placeholder="Enter lastname"
-                name="lastname"
-                id="lastname"
-              ></input>
-
-              <label htmlFor="email">Email:</label>
-              <input
-                type="text"
-                placeholder="Enter email"
-                name="email"
-                id="email"
-              ></input>
-
-              <label htmlFor="address">Address:</label>
-              <input
-                type="text"
-                placeholder="Enter address"
-                name="address"
-                id="address"
-              ></input>
-
-               <div className='price'>Total amount: {numProducts}</div>
-              <div className='individualTotal'>Total price: {totalPrice()}</div>
-              <input type="submit" className="submit" value="SUBMIT" ></input>
-
-              </div>
-              
-            </form>
-           
-            </div>
+          <FormMy
+            numProducts={numProducts}
+            totalPrice = {totalPrice}
+           />
+          </div>
            
       );
     }
